29b8f365b881e58dadc81312f66a24beb583540f
wanetty <eduard.gonzalez.moreno@est.fib.upc.edu>
2016-10-26 20:20:28 +0200

    Cambios toda la clase
    Hay que hacer un import desde eclipse de toda la carpeta Prop

---
+++ Prop/src/Palabra.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Palabra {
+
+	private String palabra;
+	private boolean funcional;
+	
+	//constructoras
+	public Palabra(){
+		palabra = "";
+		funcional = false;	
+	}
+	
+	public Palabra (String pal) throws IOException{
+		palabra = pal;
+		funcional = esfuncional();
+		
+	}
+	
+	
+	//modificadora 
+	
+	public void modificar (String pal) throws IOException{
+		
+		palabra = pal;
+		funcional = esfuncional();
+		
+	} 
+	
+	
+	//consultoras
+	
+	
+	public boolean son_iguales(Palabra pal){
+		
+		return palabra.contentEquals(pal.palabra);
+		
+	}
+	
+	public boolean esfuncional() throws IOException {
+		return comproba_funcional();
+	}
+	
+	
+	//retorna la palabra.
+	public String palabra() {
+		
+		return palabra;
+		
+		
+	}
+	public void escribir(){
+		
+		System.out.print(palabra);
+		
+	}
+	
+	private boolean comproba_funcional() throws IOException{
+		
+		BufferedReader in = new BufferedReader(new FileReader("empty.cat"));
+		String funcional = in.readLine();
+		
+		while (funcional != null){
+		
+			if(funcional.equals(palabra)){
+				
+				in.close();
+				return true;
+			} 
+			funcional = in.readLine();
+			
+		}
+		in.close();
+		in = new BufferedReader(new FileReader("empty.sp"));
+		funcional = in.readLine();
+	
+		while (funcional != null){
+			
+			if(funcional.equals(palabra)){
+				
+				in.close();
+				return true;
+			} 
+			funcional = in.readLine();
+			
+		}
+		in.close();
+		in = new BufferedReader(new FileReader("empty.eng"));
+		funcional = in.readLine();
+	
+		while (funcional != null){
+			
+			if(funcional.equals(palabra)){
+				
+				in.close();
+				return true;
+			} 
+			funcional = in.readLine();
+		}
+		in.close();
+		return false;
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+}
---
+++ Prop/src/driver.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class driver {
+	
+	
+	public static void main (String[] args) throws IOException{
+		
+		Palabra p = new Palabra("ultim");
+		
+		system
+		
+	
+	}
+
+}
